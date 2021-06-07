9a088271d3741fed6aa88b0fa18c7f025751053c
omarabdo997 <omarabdo997@gmail.com>
2021-06-08 01:58:49 +0200

    finish gui main functionalities

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 import sys
-from main_window import MainWindow
+from main_widget import MainWidget
 from PySide2.QtWidgets import QApplication 
 
 ui_file_path = "main_widget.ui"
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
-    main_window = MainWindow(ui_file_path)
-    main_window.show()
+    main_widget = MainWidget(ui_file_path)
+    main_widget.show()
     app.exec_()
     sys.exit(0)
---
+++ main_widget.py
@@ -0,0 +1,66 @@
+from PySide2.QtWidgets import QWidget, QFileDialog
+from PySide2.QtGui import QWindow
+from PySide2.QtCore import Qt
+from PySide2.QtUiTools import QUiLoader
+import subprocess
+
+
+class MainWidget(QWidget):
+    def __init__(self, ui_file_path):
+        super(MainWidget, self).__init__()
+        self._imported_pdf = None
+        self.ui_file_path = ui_file_path
+        self._setup_ui(ui_file_path)
+        self._ui.setWindowTitle("PDF-OCR")
+
+    def _load_ui_file(self, ui_file_path):
+        loader = QUiLoader()
+        self._ui = loader.load(ui_file_path)
+    
+    def _setup_ui(self, ui_file_path):
+        self._load_ui_file(ui_file_path)
+        self._ui.export_button.setDisabled(True)
+        self._ui.import_button.clicked.connect(self._on_import_button_clicked)
+        self._ui.export_button.clicked.connect(self._on_export_button_clicked)
+
+    def _load_pdf(self, pdf_file_path):
+        subprocess.Popen(["xdg-open {}".format(pdf_file_path)], shell=True)
+    
+    def _color_label(self, label, color):
+        label.setStyleSheet("color: {}".format(color))
+    
+    def _on_import_button_clicked(self):
+        pdf, _ = QFileDialog.getOpenFileUrl(self, "Import PDF", "", "PDF Files (*.pdf)")
+        pdf_path = pdf.path()
+        print("the pdf_path is",pdf_path)
+        if not pdf_path:
+            self._ui.import_label.setText("PDF file not loaded!")
+            self._color_label(self._ui.import_label, "red")
+            return
+        self._ui.import_label.setText("PDF file loaded successfully!")
+        self._color_label(self._ui.import_label, "blue")
+        self._ui.export_label.setText("")
+        self._ui.export_button.setDisabled(False)
+        self._imported_pdf = pdf_path
+    
+    def _on_export_button_clicked(self):
+        pdf_path, _ = QFileDialog.getSaveFileName(self, "Save PDF", "", "PDF Files (*.pdf)")
+        print("the new pdf_path is",pdf_path)
+        if not pdf_path:
+            self._ui.export_label.setText("PDF file not saved!")
+            self._color_label(self._ui.export_label, "red")
+            return
+        self._ui.export_label.setText("PDF file saved successfully!")
+        self._color_label(self._ui.export_label, "blue")
+        self._ui.export_button.setDisabled(True)
+        self._exported_pdf = pdf_path
+        self._load_pdf("/home/omar/Downloads/Mancala.pdf")
+    
+    def show(self):
+        self._ui.show()
+
+
+
+
+
+
