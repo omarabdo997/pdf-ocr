bc3ed7ba56feee0e31b5f3d08d5ad258e07038bd
omarabdo997 <omarabdo997@gmail.com>
2021-06-08 00:20:13 +0200

    add gui ui file

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 import sys
 from main_window import MainWindow
-from PySide2.QtWidgets import QMainWindow, QApplication 
+from PySide2.QtWidgets import QApplication 
 
+ui_file_path = "main_widget.ui"
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
-    main_window = MainWindow()
+    main_window = MainWindow(ui_file_path)
     main_window.show()
     app.exec_()
     sys.exit(0)
--- main_window.py
+++ main_window.py
@@ -1,10 +1,23 @@
-import sys
-from PySide2.QtWidgets import QMainWindow, QApplication 
+from PySide2.QtWidgets import QWidget
+from PySide2.QtUiTools import QUiLoader
 
 
-class MainWindow(QMainWindow):
-    def __init__(self):
+class MainWindow(QWidget):
+    def __init__(self, ui_file_path):
         super(MainWindow, self).__init__()
+        self._setup_ui(ui_file_path)
+        self._ui.setWindowTitle("PDF-OCR")
+
+    def _load_ui_file(self, ui_file_path):
+        loader = QUiLoader()
+        self._ui = loader.load(ui_file_path)
+    
+    def _setup_ui(self, ui_file_path):
+        self._load_ui_file(ui_file_path)
+    
+    def show(self):
+        self._ui.show()
+
 
 
 
