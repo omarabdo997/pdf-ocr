2d188e8fb2e538f0c0574508c6b0b17b04d06f40
Abdallah-Elshamy <abdallah.k.elshamy@gmail.com>
2021-06-08 13:48:43 +0200

    Add a function to clean images folder after rasterization

--- utils.py
+++ utils.py
@@ -3,3 +3,6 @@ import os
 def rasterize(pdf_url):
     os.system("gs -dSAFER -dBATCH -dNOPAUSE -sDEVICE=pgmraw -r150 -dTextAlphaBits=4 " 
               + "-sOutputFile=\'images/paper-%00d.jpg\' \"" + pdf_url+"\"")
+
+def clean_images():
+    os.system("rm images/*.jpg")
