e4637e8b7b6179f5cc5dcb6f26c424b5e767c774
Abdallah-Elshamy <abdallah.k.elshamy@gmail.com>
2021-06-08 14:23:33 +0200

    Rasterize into JPEG format

--- utils.py
+++ utils.py
@@ -1,8 +1,8 @@
 import os
 
 def rasterize(pdf_url):
-    os.system("gs -dSAFER -dBATCH -dNOPAUSE -sDEVICE=pgmraw -r150 -dTextAlphaBits=4 " 
-              + "-sOutputFile=\'images/paper-%00d.jpg\' \"" + pdf_url+"\"")
+    os.system("gs -dSAFER -dBATCH -dNOPAUSE -sDEVICE=jpeg -r150 -dTextAlphaBits=4 " 
+              + "-sOutputFile=\'images/paper-%00d.jpeg\' \"" + pdf_url+"\"")
 
 def clean_images():
-    os.system("rm images/*.jpg")
+    os.system("rm images/*.jpeg")
