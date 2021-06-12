5eba93df6d184c7bb69bee337d259d783f356a24
eslam <esla.ahme@hotmail.com>
2021-06-12 23:34:44 +0200

    merge function

--- utils.py
+++ utils.py
@@ -1,4 +1,5 @@
 import os
+import PyPDF2
 
 
 def rasterize(pdf_url):
@@ -17,3 +18,34 @@ def images_ocr_pdf():
     os.system(f"mv result.pdf ../")
     parent_dir = os.path.dirname(os.getcwd())
     os.chdir(parent_dir)
+
+
+def merge_pdf(input, ocr_output, sav_path):
+    """
+        Combine to pdf files and save the output
+        input: original pdf path
+        ocr_output: the output pdf of ocr function path
+        save_path: save path
+    """
+    pdf1File = open(ocr_output, 'rb')
+    pdf2File = open(input, 'rb')
+
+    pdf1Reader = PyPDF2.PdfFileReader(pdf1File)
+    pdf2Reader = PyPDF2.PdfFileReader(pdf2File)
+
+    pdfWriter = PyPDF2.PdfFileWriter()
+
+    for page in range(pdf1Reader.numPages):
+        pageObj = pdf1Reader.getPage(page)
+        pageObj.mergePage(pdf2Reader.getPage(page))
+        pdfWriter.addPage(pageObj)
+
+    pdfOutputFile = open(sav_path, 'wb')
+    pdfWriter.write(pdfOutputFile)
+
+    # Close all the files - Created as well as opened
+    pdfOutputFile.close()
+    pdf1File.close()
+    pdf2File.close()
+    # remove ocr output pdf
+    os.remove(ocr_output)
